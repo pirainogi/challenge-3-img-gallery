570f9d29469b943a81f1f78422495d828a5a0018
pirainogi <pirainogi@gmail.com>
2019-07-19 11:51:06 -0400

    fetching pics of dogs

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,32 @@
-console.log('hello world!');
+window.addEventListener('DOMContentLoaded', (e) => {
+    console.log('DOM fully loaded and parsed');
 
+  let dogsContainer = document.getElementById('minidogs')
+
+  fetchDogs = () => {
+    fetch('https://dog.ceo/api/breed/samoyed/images/random/4')
+    .then(r => r.json())
+    .then(dogs => {
+      let html = dogs.message.map(dog => {
+        return dogHTML(dog)
+      })
+      // console.log(html);
+      dogsContainer.innerHTML += html
+    })
+  }
+
+  dogHTML = (url) => {
+    return `
+    <div class="column">
+      <img src="${url}" alt="Dog">
+    </div>
+    `
+  }
+  //
+  // photoClick = () => {
+  //   console.log("I clicked a photo");
+  // }
+
+  fetchDogs()
+
+}); // end of dom content loaded
