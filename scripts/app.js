d8399aa4513e885bdd2faaea094f26528d8f444a
pirainogi <pirainogi@gmail.com>
2019-07-21 15:40:12 -0400

    click, fetch more btn, and big img

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,9 @@ window.addEventListener('DOMContentLoaded', (e) => {
     console.log('DOM fully loaded and parsed');
 
   let dogsContainer = document.getElementById('minidogs')
+  let closeBtn = document.querySelector(".closebtn")
+  let bigImg = document.getElementById('expandedImg')
+  // let fetchBtn = document.getElementById('fetch')
 
   fetchDogs = () => {
     fetch('https://dog.ceo/api/breed/samoyed/images/random/4')
@@ -11,25 +14,32 @@ window.addEventListener('DOMContentLoaded', (e) => {
         return dogHTML(dog)
       })
       // console.log(html);
-      dogsContainer.innerHTML += html.join('')
+      dogsContainer.innerHTML = html.join('')
     })
   }
 
   dogHTML = (url) => {
     return `
     <div class="column">
-      <img src="${url}" alt="Dog" id="${url.slice(-8, -4)}" onclick="photoClick(id)">
+      <img src="${url}" alt="Dog" id="${url.slice(-8, -4)}" onclick="photoClick(src)">
     </div>
     `
   }
 
-  photoClick = (id) => {
-    console.log("I clicked a photo", id);
-    
+  photoClick = (url) => {
+    // console.log("I clicked a photo", url);
+    bigImg.src = `${url}`
+    bigImg.parentElement.style.display = "block"
+    closeBtn.style.display = "block"
   }
 
   closePhoto = () => {
-    console.log("close dat photo plz");
+    // console.log("close dat photo plz");
+    bigImg.parentElement.style.display = "none";
+  }
+
+  fetchNewDogs = () => {
+    fetchDogs()
   }
 
   fetchDogs()
